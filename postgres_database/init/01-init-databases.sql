795dac15996b96568d1b90d91feae437007f84fe
kkvrivishvili <kote@outlook.com>
2025-06-28 17:14:03 -0300

    update

--- postgres_database/init/01-init-databases.sql
+++ postgres_database/init/01-init-databases.sql
@@ -40,16 +40,29 @@ $$;
 
 -- Asegurarse de que el esquema público tenga los permisos correctos
 GRANT ALL ON SCHEMA public TO nooble_admin;
-GRANT USAGE, CREATE ON SCHEMA public TO authentik_user;
+GRANT CREATE, USAGE ON SCHEMA public TO authentik_user WITH GRANT OPTION;
 
 -- Configurar permisos predeterminados para futuras tablas
 ALTER DEFAULT PRIVILEGES IN SCHEMA public 
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO authentik_user;
 
 -- Otorgar todos los permisos necesarios en la base de datos
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO authentik_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO authentik_user;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO authentik_user;
+GRANT ALL PRIVILEGES ON DATABASE authentik TO authentik_user;
+ALTER DATABASE authentik OWNER TO authentik_user;
+
+-- Grant all privileges on the public schema
+GRANT ALL ON SCHEMA public TO authentik_user WITH GRANT OPTION;
+
+-- Grant all privileges on all objects in the public schema
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO authentik_user WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO authentik_user WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO authentik_user WITH GRANT OPTION;
+
+-- Set default privileges for future objects
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO authentik_user WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO authentik_user WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO authentik_user WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TYPES TO authentik_user WITH GRANT OPTION;
 
 -- Mensaje de finalización exitosa
 DO $$
